9e45fdd2b1d8482277772cf25b05419c306f4286
Zeno Saletti <zeno.saletti@studenti.unitn.it>
2024-01-17 09:21:22 +0100

    gnomesort and other low-performance algorithms

--- sort/sort.cc
+++ sort/sort.cc
@@ -46,3 +46,67 @@ void insertionsort(int A[], int n)
         A[j] = tmp;
     }
 }
+
+void bubblesort(int A[], int n)
+{
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (A[j] > A[j + 1])
+                swap(A[j], A[j + 1]);
+        }
+    }
+}
+
+/*
+ * The problem with bubble sort is
+ * that it is incapable of detecting
+ * portions of the array that are
+ * already sorted. The following
+ * optimization, however, does not
+ * decrease the time complexity
+ * (yes, bubble sort still sucks)
+ */
+void bubblesort2(int A[], int n)
+{
+    bool sorted = false;
+
+    for (int i = n - 1; i > 0 && !sorted; i--) {
+
+        sorted = true;
+
+        for (int j = 0; j < i; j++) {
+
+            if (A[j] > A[j + 1]) {
+                swap(A[j], A[j + 1]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+/*
+ * Carrying out a sorting operation
+ * with only one 'while' cycle may
+ * be regarded as the most remarkable
+ * characteristic of this algorithm.
+ * Don't be fooled by this, however:
+ * you should convince yourself that
+ * a time complexity of O(n) will
+ * be a dream. According to the
+ * authors, this is a common algorighm
+ * used by garden gnomes to sort their
+ * rows of flower pots
+ */
+void gnomesort(int A[], int n)
+{
+    int i = 0;
+
+    while (i < n) {
+        if (i == 0 || A[i] >= A[i - 1]) {
+            i++;
+        } else {
+            swap(A[i], A[i - 1]);
+            i--;
+        }
+    }
+}
--- sort/sort.h
+++ sort/sort.h
@@ -2,6 +2,10 @@
 #define SORT_H
 
 void selectionsort(int A[], int n);
+void bubblesort(int A[], int n);
 void insertionsort(int A[], int n);
+void gnomesort(int A[], int n);
+
+void bubblesort2(int A[], int n);
 
 #endif
